6b48a8f833ef8501a807b7891eef94bae6b488a3
moviecove <404nullzone@gmail.com>
2026-02-07 08:31:11 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bgPrimary: "#232D26",
+        bgSecondary: "#6B746B",
+        bgLight: "#CCD4C9",
+
+        accentPrimary: "#576E45",
+        accentSecondary: "#899C8F",
+        accentHighlight: "#AAC64B",
+      },
+    },
+  },
+  plugins: [],
+};
